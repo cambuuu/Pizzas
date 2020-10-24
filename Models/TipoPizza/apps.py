d0d92916b8339e815376ab4b925938b8b90548d5
Ricardo Sepulveda Leon <ricardosepulveda86@gmail.com>
2020-10-24 19:18:39 -0300

    v1 de pizzas

---
+++ Models/TipoPizza/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TipopizzaConfig(AppConfig):
+    name = 'Models.TipoPizza'
---
+++ Views/HomeView.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+
+class HomeView():
+
+    def home(self):
+        plantilla = get_template('index.html')
+        return HttpResponse(plantilla.render())
+    def pagina1(self):
+        return HttpResponse('hola nuevamente aweonanos')
+    def pagina2(self, parametro1):
+        return HttpResponse('hola aweonaos desde: ' + str(parametro1))
+    def pagina3(self, parametro1, parametro2):
+        return HttpResponse('Hola desde XD :' + str(parametro1) + ' - ' + str(parametro2))
+
+    def formulario(self):
+        plantilla = get_template('formulario.html')
+        return HttpResponse(plantilla.render())
